87cc07203dac6ddd29686cfab682429a2d71224c
Syed Imad Muzaffar <imad.muza@gmail.com>
2026-02-19 12:58:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,19 @@
 import streamlit as st
 import joblib
 
-
 model = joblib.load('spam_model.pkl')
-
 
 st.title("🚨 SMS Spam Detector")
 st.write("Are you unsure if a text message is a scam? Paste it below to find out!")
 
-
-# Notice the "" at the end? That means the box starts completely empty!
 user_input = st.text_area("Enter the text message here:", "")
 
-
 if st.button("Check Message"):
-    
     
     if user_input.strip() == "":
         st.warning("Please type or paste a message into the box first!")
     else:
-     
         prediction = model.predict([user_input])[0]
-        
-       
         if prediction == 'spam':
             st.error("🛑 ALERT: This message is SPAM!")
         else:
